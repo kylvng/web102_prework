b60245786eeb3558fc0aeda1e0323d04484754f7
KyleVong <121789633+kylvng@users.noreply.github.com>
2024-02-01 00:54:12 -0500

    Completed Challenge 7

--- index.js
+++ index.js
@@ -178,7 +178,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
-let firstGame, secondGame = [firstGame, secondGame, ...sortedGame];
-// create a new element to hold the name of the top pledge game, then append it to the correct element
+let [{ name: firstName }, { name: secondName }, ...restOfGames] = sortedGames;
 
-// do the same for the runner up item
+// create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameElement = document.createElement("div");
+firstGameElement.innerHTML = `${firstName}`;
+firstGameContainer.appendChild(firstGameElement);
+
+// do the same for the runner-up item
+const secondGameElement = document.createElement("div");
+secondGameElement.innerHTML = `${secondName}`;
+secondGameContainer.appendChild(secondGameElement);
